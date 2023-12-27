7bde1bee52472f0bb23d2ed9705b71d83340571b
Matt Filetto <matt.filetto@gmail.com>
2023-12-27 12:12:50 -0800

    hardcode HasAlertSlider to false for now.
    * PixelDisplayService crashes cuz of this impl somehow (i know it makes no sense).so we are dropping it for now in  fw/base.
    
    Signed-off-by: Matt Filetto <matt.filetto@gmail.com>

--- src/com/crdroid/settings/fragments/Notifications.java
+++ src/com/crdroid/settings/fragments/Notifications.java
@@ -74,8 +74,7 @@ public class Notifications extends SettingsPreferenceFragment implements
         final Resources res = mContext.getResources();
 
         mAlertSlider = (Preference) prefScreen.findPreference(ALERT_SLIDER_PREF);
-        boolean mAlertSliderAvailable = res.getBoolean(
-                com.android.internal.R.bool.config_hasAlertSlider);
+        boolean mAlertSliderAvailable = false;
         if (!mAlertSliderAvailable)
             prefScreen.removePreference(mAlertSlider);
 
@@ -152,8 +151,7 @@ public class Notifications extends SettingsPreferenceFragment implements
                     List<String> keys = super.getNonIndexableKeys(context);
                     final Resources res = context.getResources();
 
-                    boolean mAlertSliderAvailable = res.getBoolean(
-                            com.android.internal.R.bool.config_hasAlertSlider);
+                    boolean mAlertSliderAvailable = false;
                     if (!mAlertSliderAvailable)
                         keys.add(ALERT_SLIDER_PREF);
 
